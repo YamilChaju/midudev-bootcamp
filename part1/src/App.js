3be743ea3adcf7c01b478718ecda116a1ca533c1
Yamil <yamilchaju@gmail.com>
2022-07-09 12:26:16 -0300

    commit3

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import Mensaje from './Mensaje';
 //App es un componente, tiene que ir con mayuscula, retorna una especie de html
 //pero no es html, es jsx, el compilador lo transforma en js
